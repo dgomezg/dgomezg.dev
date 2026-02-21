5192a87ef277732ef19fcfc05fe695b4e9333718
David Gómez G. <dgomezg@gmail.com>
2026-02-21 18:30:41 +0100

    Added list of conferecens for each talk

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,15 @@ import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 
+import alpinejs from '@astrojs/alpinejs';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://dgomezg.dev',
+
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+
+  integrations: [alpinejs()]
 });
